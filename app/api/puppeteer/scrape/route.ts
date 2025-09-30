f4f11041c66d2093741e48444fb3808b27f39803
virapat-40 <virapat@40.co.th>
2025-09-30 14:55:51 +0700

    1 gb

--- app/api/puppeteer/scrape/route.ts
+++ app/api/puppeteer/scrape/route.ts
@@ -28,7 +28,7 @@ export async function POST(request: NextRequest) {
         '--no-zygote',
         '--disable-gpu',
         '--memory-pressure-off',
-        '--max_old_space_size=512'
+        '--max_old_space_size=1024'
       ]
     })
 
